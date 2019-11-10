4136314b39c42097a9988ae6b4bf5eac9af785b9
StafPers <mail@staffanpersson.net>
2019-11-10 15:55:57 +0100

    Add ConsoleColors

---
+++ Lycan/src/Core/ConsoleColors.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <Windows.h> // Prob do something about this
+
+namespace Lycan
+{
+	namespace Core
+	{
+		enum eConsoleColors
+		{
+			White        = FOREGROUND_BLUE  | FOREGROUND_RED | FOREGROUND_GREEN,
+			LightRed     = FOREGROUND_RED   | FOREGROUND_INTENSITY,
+			DarkRed      = FOREGROUND_RED,
+			LightGreen   = FOREGROUND_GREEN | FOREGROUND_INTENSITY,
+			DarkGreen    = FOREGROUND_GREEN,
+			LightBlue    = FOREGROUND_BLUE  | FOREGROUND_INTENSITY,
+			DarkBlue     = FOREGROUND_BLUE,
+			LightYellow  = FOREGROUND_GREEN | FOREGROUND_RED  | FOREGROUND_INTENSITY,
+			DarkYellow   = FOREGROUND_GREEN | FOREGROUND_RED,
+			LightMagenta = FOREGROUND_BLUE  | FOREGROUND_RED  | FOREGROUND_INTENSITY,
+			DarkMagenta  = FOREGROUND_BLUE  | FOREGROUND_RED,
+			LightCyan    = FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY,
+			DarkCyan     = FOREGROUND_BLUE  | FOREGROUND_GREEN
+		};
+	}
+}
