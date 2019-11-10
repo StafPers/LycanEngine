517876a373186913595c6064fd4e6c7fed815634
StafPers <mail@staffanpersson.net>
2019-11-10 15:56:41 +0100

    Add UniquePtr

---
+++ Lycan/src/Core/UniquePtr.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include "Util.h"
+
+namespace Norselong
+{
+	namespace Core
+	{
+		template< class T >
+		class UniquePtr
+		{
+		public:
+
+			UniquePtr()
+				: m_pObj( nullptr )
+			{}
+
+			UniquePtr( T* _pObj )
+				: m_pObj( _pObj )
+			{}
+
+			UniquePtr( UniquePtr< T >&& _rSource )
+				: m_pObj( _rSource.m_pObj )
+			{
+				_rSource.m_pObj = nullptr;
+			}
+
+			~UniquePtr()
+			{
+				delete m_pObj;
+			}
+
+			UniquePtr( const UniquePtr< T >& ) = delete;
+			UniquePtr< T >& operator=( const UniquePtr< T >& ) = delete;
+
+			UniquePtr< T >& operator=( UniquePtr< T >&& _rSource )
+			{
+				if( this != &_rSource )
+				{
+					delete m_pObj;
+					m_pObj = _rSource.m_pObj;
+					_rSource.m_pObj = nullptr;
+				}
+
+				return *this;
+			}
+
+			T* operator->         ( void )       { return m_pObj; }
+			T& operator*          ( void )       { return *m_pObj; }
+			explicit operator bool( void ) const { return m_pObj != nullptr; }
+
+			inline T* Get         ( void )       { return m_pObj; }
+
+			T* Release( void )
+			{
+				T* pTemp = m_pObj;
+				m_pObj = nullptr;
+				return pTemp;
+			}
+
+		private:
+
+			T* m_pObj;
+
+		};
+
+		template< typename T, typename... Args >
+		UniquePtr< T > MakeUnique( Args&&... args )
+		{
+			return UniquePtr< T >( new T( Forward( args... ) ) );
+		}
+	}
+}
