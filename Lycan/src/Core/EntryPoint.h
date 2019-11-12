87bf477e581fb5759b0d18536449786eec936e42
StafPers <mail@staffanpersson.net>
2019-11-12 22:07:09 +0100

    Update clock in main

--- Lycan/src/Core/EntryPoint.h
+++ Lycan/src/Core/EntryPoint.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "Clock.h"
+
 #ifdef LC_PLATFORM_WINDOWS
 
 extern Lycan::Application* Lycan::CreateApplication();
@@ -7,7 +9,15 @@ extern Lycan::Application* Lycan::CreateApplication();
 int main( int argc, char** argv )
 {
 	auto app = Lycan::CreateApplication();
-	app->Run();
+	app->Initialize();
+	
+	while( true )
+	{
+		Lycan::Core::Clock::Instance().Update();
+		app->Update();
+		app->Render();
+	}
+
 	delete app;
 
 	return 0;
